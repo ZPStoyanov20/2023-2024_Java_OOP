b824b0806490b12aa8bfdee261020e2461e765db
Зафир Пламенов Стоянов <ZPStoyanov20@codingburgas.bg>
2023-10-10 09:27:04 +0300

    Task_2 Done!

---
+++ java_10_10_2023/src/Zadacha1/Main.java
@@ -0,0 +1,7 @@
+package Zadacha1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello Java!%nThis is another line");
+    }
+}
---
+++ java_10_10_2023/src/Zadacha4/Main.java
@@ -0,0 +1,12 @@
+package Zadacha4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 1; i <= 5; i++)
+                System.out.printf("%d ", i);
+            System.out.printf("%n");
+        }
+    }
+}
